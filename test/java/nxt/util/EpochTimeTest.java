0b50056be5fc1faf2fe2bed9a091c06fbbead521
ScripterRon <Ronald.Hoffman6@gmail.com>
2017-01-17 18:37:04 -0500

    Fix EpochTimeTest

--- test/java/nxt/util/EpochTimeTest.java
+++ test/java/nxt/util/EpochTimeTest.java
@@ -25,14 +25,16 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class EpochTimeTest {
+    
+    private static final int testTime = 1333920;
 
     @Test
     public void simple() {
-        long time = Convert.fromEpochTime(47860355);
+        long time = Convert.fromEpochTime(testTime);
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        Assert.assertEquals("01/06/2015 10:32:34", dateFormat.format(new Date(time)));
-        Assert.assertEquals(47860355, Convert.toEpochTime(time));
+        Assert.assertEquals("16/01/2017 10:31:59", dateFormat.format(new Date(time)));
+        Assert.assertEquals(testTime, Convert.toEpochTime(time));
     }
 
 }
