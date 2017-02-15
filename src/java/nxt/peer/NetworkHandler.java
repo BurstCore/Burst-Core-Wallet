dc4fc077d48a0f8f0891250bf33847e2be370d5e
ScripterRon <Ronald.Hoffman6@gmail.com>
2017-02-15 14:24:12 -0500

    Handle pending peer disconnect

--- src/java/nxt/peer/NetworkHandler.java
+++ src/java/nxt/peer/NetworkHandler.java
@@ -31,7 +31,6 @@ import java.net.InetSocketAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.StandardSocketOptions;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
@@ -499,6 +498,9 @@ public final class NetworkHandler implements Runnable {
          * @param   removeOps           Operations to be removed
          */
         void update(int addOps, int removeOps) {
+            if (peer.isDisconnectPending()) {
+                return;
+            }
             if (Thread.currentThread() == listenerThread) {
                 if (key.isValid()) {
                     key.interestOps((key.interestOps() | addOps) & (~removeOps));
@@ -644,6 +646,7 @@ public final class NetworkHandler implements Runnable {
                 } else if (connectionMap.get(remoteAddress.getAddress()) != null) {
                     channel.close();
                     Logger.logDebugMessage("Connection already established with " + hostAddress + ", disconnecting");
+                    peer.setDisconnectPending();
                     Peers.peersService.execute(peer::disconnectPeer);
                 } else {
                     channel.configureBlocking(false);
@@ -705,6 +708,7 @@ public final class NetworkHandler implements Runnable {
                             if (keyEvent != null) {
                                 keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                             }
+                            peer.setDisconnectPending();
                             Peers.peersService.execute(peer::disconnectPeer);
                         }
                         break;
@@ -726,6 +730,7 @@ public final class NetworkHandler implements Runnable {
                         if (keyEvent != null) {
                             keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                         }
+                        peer.setDisconnectPending();
                         Peers.peersService.execute(peer::disconnectPeer);
                         break;
                     }
@@ -736,6 +741,7 @@ public final class NetworkHandler implements Runnable {
                         if (keyEvent != null) {
                             keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                         }
+                        peer.setDisconnectPending();
                         Peers.peersService.execute(peer::disconnectPeer);
                         break;
                     }
@@ -775,6 +781,7 @@ public final class NetworkHandler implements Runnable {
             if (keyEvent != null) {
                 keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
             }
+            peer.setDisconnectPending();
             Peers.peersService.execute(peer::disconnectPeer);
         }
     }
@@ -821,6 +828,7 @@ public final class NetworkHandler implements Runnable {
                         if (keyEvent != null) {
                             keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                         }
+                        peer.setDisconnectPending();
                         Peers.peersService.execute(peer::disconnectPeer);
                         break;
                     } catch (Exception exc) {
@@ -830,6 +838,7 @@ public final class NetworkHandler implements Runnable {
                         if (keyEvent != null) {
                             keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                         }
+                        peer.setDisconnectPending();
                         Peers.peersService.execute(peer::disconnectPeer);
                         break;
                     }
@@ -854,6 +863,7 @@ public final class NetworkHandler implements Runnable {
             if (keyEvent != null) {
                 keyEvent.update(0, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
             }
+            peer.setDisconnectPending();
             Peers.peersService.execute(peer::disconnectPeer);
         }
     }
