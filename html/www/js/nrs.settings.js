6369b51dbb1886e0935115a8f5eb32b3f6a6e767
lyaffe <lyaffe@gmail.com>
2017-02-25 22:29:30 +0200

    Make 8 decimals the default for JUP

--- html/www/js/nrs.settings.js
+++ html/www/js/nrs.settings.js
@@ -39,7 +39,7 @@ var NRS = (function(NRS, $) {
         "exchange_coin0": "BTC",
         "exchange_coin1": "LTC",
         "exchange_coin2": "ETH",
-		"max_nxt_decimals": "2"
+		"max_nxt_decimals": "8"
 	};
 
 	NRS.defaultColors = {
