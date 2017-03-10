8fae59890903b0815635e11573f48cfceadd5fd3
Jean-Luc Picard <jlp@mailbox.org>
2017-03-10 22:39:14 +0200

    version, updated default peers

--- src/java/nxt/Nxt.java
+++ src/java/nxt/Nxt.java
@@ -79,7 +79,7 @@ import java.util.Properties;
 
 public final class Nxt {
 
-    public static final String VERSION = "2.0.1e";
+    public static final String VERSION = "2.0.2e";
     public static final String APPLICATION = "Ardor";
 
     private static volatile Time time = new Time.EpochTime();
