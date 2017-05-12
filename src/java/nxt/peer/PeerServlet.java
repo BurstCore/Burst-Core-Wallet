93078073c56a13ba0084c7a5bca5ee57d3ff6569
Dom <misc-github@talk2dom.com>
2017-05-12 16:51:09 +0100

    re-fix peer protocol check logic

--- src/java/nxt/peer/PeerServlet.java
+++ src/java/nxt/peer/PeerServlet.java
@@ -258,7 +258,7 @@ public final class PeerServlet extends WebSocketServlet {
         try (CountingInputReader cr = new CountingInputReader(inputReader, Peers.MAX_REQUEST_SIZE)) {
             JSONObject request = (JSONObject)JSONValue.parseWithException(cr);
             peer.updateDownloadedVolume(cr.getCount());
-            if (request.get("protocol") == null || !((Number)request.get("protocol")).equals("B1")) {
+            if (request.get("protocol") == null || !request.get("protocol").equals("B1")) {
                 Logger.logDebugMessage("Unsupported protocol " + request.get("protocol"));
                 return UNSUPPORTED_PROTOCOL;
             }
